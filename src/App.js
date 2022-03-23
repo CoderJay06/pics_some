069e04ac06b244ded51597f45ce9d18b437029c0
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2022-03-23 01:11:03 -0400

    Setup file structure and configure react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* header here */}
+      <Header />
+      {/*routes here */}
+      <Routes>
+        <Route exact path="/" component={Photos} />
+        <Route path="/" component={Cart} />
+      </Routes>
     </div>
   );
 }
